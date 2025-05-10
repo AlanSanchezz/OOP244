3b6459eba91a935181df59774542d7316aa76f05
Alan Sanchez <alanbega19@gmail.com>
2025-05-09 22:22:12 -0400

    added DIY

---
+++ DIY/DIY/Hello.cpp
@@ -0,0 +1,8 @@
+//
+//  Hello.cpp
+//  DIY
+//
+//  Created by Alan Sanchez on 2025-05-07.
+//
+
+#include <stdio.h>
---
+++ DIY/DIY/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DIY
+//
+//  Created by Alan Sanchez on 2025-05-07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Workshop DIY!\n";
+    return 0;
+}
