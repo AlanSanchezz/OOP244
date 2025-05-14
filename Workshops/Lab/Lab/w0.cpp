356bb97cd6fb0350534dc64e6c250d17d7fb79fd
Alan Sanchez <alanbega19@gmail.com>
2025-05-14 19:42:23 -0400

    added w0

---
+++ Workshops/Lab/Lab/w0.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Lab
+//
+//  Created by Alan Sanchez on 2025-05-14.
+//
+/*
+ #### Workshop Zero ####
+ Subject and Section:
+ Your subject and section goes here (example: AAA999 NXX)
+ Name:
+ Your full name goes here
+ Student Number:
+ Your student number goes here
+ Email:
+ Your email goes here
+ */
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    cout << "#### Workshop Zero ####\nSubject and Section:\n(OOP244 NAA)\nName:\nAlan Yahir Garduno Sanchez\nStudent Number:\n121720247\nEmail:\naygarduno-sanchez@myseneca.ca\n";
+    return 0;
+}
