771daa839e1080cbd68fe1a8799e5666ff7ec5d5
Alan Sanchez <alanbega19@gmail.com>
2025-06-12 23:07:44 -0400

    Added W4

--- Workshops/W4/Account.cpp
+++ Workshops/W4/Account.cpp
@@ -4,16 +4,15 @@
 // File	Account.cpp
 // Version 0.5
 // Date	2025/06/03
-// Partialy implemented by	Fardad Soleimanloo
-// to be completed by Alan Sanchez
+// Partially implemented by	Fardad Soleimanloo
+// To be completed by Alan Sanchez
 // You may modify any code in this file to suit your requirements.
-// Description
-// 
+//
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
 // Fardad Soleimanloo  2025/06/03  Preliminary implementation
-// Alan Sanchez  2025/06/03  Worked on implementation
+// Alan Sanchez        2025/06/03  Worked on implementation
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
 
@@ -21,88 +20,210 @@
 #include "Account.h"
 
 using namespace std;
+
 namespace seneca {
- 
 
-   void Account::cpyName(const char* src) {
-      int i;
-      for (i = 0; src && src[i] && i < NameMaxLen; i++) {
-         m_holderName[i] = src[i];
-      }
-      m_holderName[i] = char(0);
-   }
-   bool Account::isValidNumber(int number) const {
-      return number >= 10000 && number <= 99999;
-   }
-   Account::Account(const char* holderName) {  
-      m_holderName[0] = char(0);
-      m_number = -1;
-      m_balance = 0.0;
-      if (holderName && holderName[0]) {
-         cpyName(holderName);
-         m_number = 0;
-      }
-   }
-   Account::Account(const char* holderName, int number, double balance ) {
-      m_holderName[0] = char(0);
-      m_number = -1;
-      m_balance = 0.0;
-      if (holderName && holderName[0] && isValidNumber(number)
-         && balance > 0) {
-         cpyName(holderName);
-         m_number = number;
-         m_balance = balance;
-      }
-   }
-   std::ostream& Account::display() const{
-      if (*this) {  
-         cout << " ";
-         cout.fill(' ');
-         cout.width(NameMaxLen);
-         cout.setf(ios::left);
-         cout << m_holderName;
-         cout.unsetf(ios::left);
-         cout << " | ";
-         cout << m_number;
-         cout << " | ";
-         cout.width(12);
-         cout.precision(2);
-         cout.setf(ios::right);
-         cout.setf(ios::fixed);
-         cout << m_balance;
-         cout.unsetf(ios::right);
-         cout << " ";
-      }
-      else if(~*this) {
-         cout << " ";
-         cout.fill(' ');
-         cout.width(30);
-         cout.setf(ios::left);
-         cout << m_holderName;
-         cout.unsetf(ios::left);
-         cout << " |  NEW  |         0.00 ";
-      }
-      else {
-         cout << " Bad Account                    | ----- | ------------ ";
-      }
-      return cout;
-   }
-   bool Account::operator~() const {
-      return m_number == 0 && m_holderName[0] != '\0';
-   }
-   Account::operator bool() const {
-      return isValidNumber(m_number) && m_holderName[0] != '\0' && m_balance >= 0.0;
-   }
-   Account::operator int() const {
-      return m_number;
-   }
-   Account::operator double() const {
-      return m_balance;
-   }
-   Account::const char*() const {
-      return m_holderName;
-   }
-
-
-
+// Helper function to copy the name safely
+void Account::cpyName(const char* src) {
+    int i;
+    for (i = 0; src && src[i] && i < NameMaxLen; i++) {
+        m_holderName[i] = src[i];
+    }
+    m_holderName[i] = '\0';
 }
+
+// Validates account number (5-digit)
+bool Account::isValidNumber(int number) const {
+    return number >= 10000 && number <= 99999;
+}
+
+// Constructor for new account (number not assigned yet)
+Account::Account(const char* holderName) {
+    m_holderName[0] = '\0';
+    m_number = -1;
+    m_balance = 0.0;
+    if (holderName && holderName[0]) {
+        cpyName(holderName);
+        m_number = 0;  // New account state
+    }
+}
+
+// Constructor for valid or invalid account based on inputs
+Account::Account(const char* holderName, int number, double balance) {
+    m_holderName[0] = '\0';
+    m_number = -1;
+    m_balance = 0.0;
+    if (holderName && holderName[0] && isValidNumber(number) && balance >= 0) {
+        cpyName(holderName);
+        m_number = number;
+        m_balance = balance;
+    }
+}
+
+// Display function outputs account info based on state
+ostream& Account::display() const {
+    if (*this) {  // Valid account
+        cout << " ";
+        cout.fill(' ');
+        cout.width(NameMaxLen);
+        cout.setf(ios::left);
+        cout << m_holderName;
+        cout.unsetf(ios::left);
+        cout << " | ";
+        cout << m_number;
+        cout << " | ";
+        cout.width(12);
+        cout.precision(2);
+        cout.setf(ios::right);
+        cout.setf(ios::fixed);
+        cout << m_balance;
+        cout.unsetf(ios::right);
+        cout << " ";
+    } 
+    else if (~*this) {  // New account
+        cout << " ";
+        cout.fill(' ');
+        cout.width(NameMaxLen);
+        cout.setf(ios::left);
+        cout << m_holderName;
+        cout.unsetf(ios::left);
+        cout << " |  NEW  |         0.00 ";
+    } 
+    else {  // Invalid account
+        cout << " Bad Account                    | ----- | ------------ ";
+    }
+    return cout;
+}
+
+// Unary operator ~: checks if account is new (number == 0)
+bool Account::operator~() const {
+    return m_number == 0 && m_holderName[0] != '\0';
+}
+
+// Prefix increment: adds $1 to balance if valid
+Account& Account::operator++() {
+    if (*this) {
+        m_balance += 1.0;
+    }
+    return *this;
+}
+
+// Postfix increment: adds $1 but returns old state
+Account Account::operator++(int) {
+    Account temp = *this;
+    ++(*this);
+    return temp;
+}
+
+// Prefix decrement: subtracts $1 if enough balance and valid
+Account& Account::operator--() {
+    if (*this && m_balance >= 1.0) {
+        m_balance -= 1.0;
+    }
+    return *this;
+}
+
+// Postfix decrement: subtracts $1 but returns old state
+Account Account::operator--(int) {
+    Account temp = *this;
+    --(*this);
+    return temp;
+}
+
+// Conversion to bool: true if valid account
+Account::operator bool() const {
+    return isValidNumber(m_number) && m_holderName[0] != '\0' && m_balance >= 0.0;
+}
+
+// Conversion to int: returns account number
+Account::operator int() const {
+    return m_number;
+}
+
+// Conversion to double: returns balance
+Account::operator double() const {
+    return m_balance;
+}
+
+// Conversion to const char*: returns holder name
+Account::operator const char*() const {
+    return m_holderName;
+}
+
+// Subscription operators for name access with wrap-around
+char& Account::operator[](int index) {
+    static char dummy = '\0';  // Dummy for invalid access
+    if (m_holderName[0] == '\0') {
+        return dummy;
+    }
+    index = index % NameMaxLen;
+    return m_holderName[index];
+}
+
+const char Account::operator[](int index) const {
+    if (m_holderName[0] == '\0') return '\0';
+    index = index % NameMaxLen;
+    return m_holderName[index];
+}
+
+// Assignment operator for int: sets account number if new and valid
+Account& Account::operator=(int number) {
+    if (~(*this)) {
+        if (isValidNumber(number)) {
+            m_number = number;
+        } else {
+            m_holderName[0] = '\0';
+            m_number = -1;
+            m_balance = 0.0;
+        }
+    }
+    return *this;
+}
+
+// Assignment operator for double: sets balance if non-negative
+Account& Account::operator=(double value) {
+    if (value >= 0.0) {
+        m_balance = value;
+    } else {
+        m_holderName[0] = '\0';
+        m_number = -1;
+        m_balance = 0.0;
+    }
+    return *this;
+}
+
+// Deposit operator += adds amount to balance if valid and non-negative
+Account& Account::operator+=(double value) {
+    if (*this && value >= 0.0) {
+        m_balance += value;
+    }
+    return *this;
+}
+
+// Withdraw operator -= subtracts amount from balance if valid and sufficient funds
+Account& Account::operator-=(double value) {
+    if (*this && value >= 0.0 && m_balance >= value) {
+        m_balance -= value;
+    }
+    return *this;
+}
+
+// Friend operator << to transfer funds from right account to left account
+Account& operator<<(Account& left, Account& right) {
+    if (&left != &right && left && right) {
+        left.m_balance += right.m_balance;
+        right.m_balance = 0.0;
+    }
+    return left;
+}
+
+// Friend operator >> to transfer funds from left account to right account
+Account& operator>>(Account& left, Account& right) {
+    if (&left != &right && left && right) {
+        right.m_balance += left.m_balance;
+        left.m_balance = 0.0;
+    }
+    return left;
+}
+
+}  // namespace seneca
--- Workshops/W4/Account.h
+++ Workshops/W4/Account.h
@@ -1,51 +1,62 @@
-/***********************************************************************
-// OOP244 Workshop 4 lab: Account Module
-//
-// File	Account.h
-// Version 0.5
-// Date	2025/06/03
-// Partialy implemented by	Fardad Soleimanloo
-// to be completed by Alan Sanchez
-// Description
-//
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-// Fardad Soleimanloo  2025/06/03  Preliminary implementation
-// Alan Sanchez  2025/06/03  Worked on implementation
-// Alan Sanchez  2025/06/03  Finalized Constructor and display function
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
 #ifndef SENECA_ACCOUNT_H_
 #define SENECA_ACCOUNT_H_
+
 #include <iostream>
 
 namespace seneca {
+
    const int NameMaxLen = 30;
+
    class Account {
       double m_balance;
       int m_number;
       char m_holderName[NameMaxLen + 1];
-      void cpyName(const char* src); // copies the src into the m_holderName array
-      bool isValidNumber(int number)const; // returns true if number is a valid account number
+
+      // Helper private methods
+      void cpyName(const char* src);               // copies src into m_holderName safely
+      bool isValidNumber(int number) const;        // validates 5-digit account number
+
    public:
 
       // Constructors
       Account(const char* holderName = nullptr);
       Account(const char* holderName, int number, double balance);
-      // Display function
-      std::ostream& display()const;
-      
-      // Operators
+
+      // Display account details
+      std::ostream& display() const;
+
+      // Unary operator to check if account is new (number == 0)
       bool operator~() const;
-      operator bool() const; // returns true if the account is valid
-      operator int() const; // returns the account number.
-      operator double() const; // returns the account balance
-      operator const char*() const;  // returns the account holder's name
 
+      // Type conversion operators
+      operator bool() const;            // true if account is valid
+      operator int() const;             // returns account number
+      operator double() const;          // returns balance
+      operator const char*() const;     // returns account holder name
 
+      // Subscription operators for name access
+      char& operator[](int index);               // allows modification
+      const char operator[](int index) const;   // read-only access
+
+      // Assignment operators
+      Account& operator=(int number);
+      Account& operator=(double value);
+
+      // Arithmetic operators
+      Account& operator+=(double value);
+      Account& operator-=(double value);
+
+      // Increment and decrement operators
+      Account& operator++();       // Prefix increment
+      Account operator++(int);     // Postfix increment
+      Account& operator--();       // Prefix decrement
+      Account operator--(int);     // Postfix decrement
+
+      // Friend functions for fund transfer operators
+      friend Account& operator<<(Account& left, Account& right);
+      friend Account& operator>>(Account& left, Account& right);
    };
-  
 
-}
+} // namespace seneca
+
 #endif // SENECA_ACCOUNT_H_
