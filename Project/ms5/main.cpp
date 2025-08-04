19178b5a6b6543aec19595943fef344d74209b64
Alan Sanchez <alanbega19@gmail.com>
2025-08-04 17:46:34 -0400

    output

--- Project/ms5/main.cpp
+++ Project/ms5/main.cpp
@@ -1,99 +1,19 @@
-/***************************************************************
-// OOP244 Project, milestone 56 : tester program
-//
-// File  ms56.cpp
-// Version 1.0
-// Author Fardad
-// Description
-//
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-//
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
 #include <iostream>
 #include "Menu.h"
 #include "Ordering.h"
 using namespace std;
 using namespace seneca;
-Ordering ordering("drinks.csv", "foods.csv");
-void test1();
-
-
-
-
 int main() {
-
-   // Validate that data loaded correctly
-   if (!ordering) {
-      std::cout << "Failed to open data files or the data files are corrupted!" << std::endl;
-      return 1; // Exit program with error code
+   cout << "Testing Invalid Ordering if bad data is in data file!" << endl;
+   Ordering badord("drink.csv", "badfood.csv");
+   if (badord) {
+      cout << "The ordering should have been invalid" << endl;
    }
-
-
-   size_t selection{}, select{};
-    Menu mainMenu("Seneca Restaurant ", "End Program");
-    mainMenu << "Order" << "Print Bill" << "Start a New Bill" << "List Foods" << "List Drinks";
-    Menu exit("You have bills that are not saved, are you sue you want to exit?", "No");
-    exit << "Yes";
-
-    do {
-      selection = mainMenu.select();
-      switch (selection) {
-      case 0:
-         select = exit.select();
-         break;
-      case 1: 
-         test1();
-         break;
-      case 2:
-
-
-         ordering.printBill(cout);
-
-         break;
-      case 3:
-         ordering.resetBill();
-         break;
-      case 4:
-         ordering.listFoods();
-         break;
-      case 5:
-         ordering.listDrinks();
-         break;
-      default:
-         cout << "This should never happen!" << endl;
-      }
-   } while (selection || !select);
-
-
+   else {
+      cout << "Passed!" << endl;
+   }
+   Ordering ord("drinks.csv", "foods.csv");
+   cout << "Testing fool proofing..............................." << endl;
+   ord.orderDrink();
    return 0;
-
-
-   
 }
-
-
-void test1() {
-   size_t selection{};
-   Menu submenu("Order Menu", "Back to main menu", 1);
-   submenu << "Food" << "Drink";
-   do {
-      selection = submenu.select();
-      switch (selection) {
-      case 1:
-         ordering.orderFood();
-         break;
-      case 2:
-         ordering.orderDrink();
-      default:
-         break;
-      }
-   } while (selection);
-
-}
-
-
-
-
